e0ba0153bee7cbf72350c6263b41254f0b46f5f7
Mamun13 <mamuncse82@gmail.com>
2022-07-04 12:35:49 +0600

    complete_toDo_list

--- src/Todo.js
+++ src/Todo.js
@@ -1,23 +1,55 @@
 import React from 'react';
-import "../src/Todo.css"
+import { useState } from 'react';
+import "../src/Todo.css";
 
 const Todolist = () => {
-  return(
-      <>
-        <section id='todo'>
-          <div className="container">
-            <div className="row p-0">
-              <div className="col-lg-4 p-0"></div>
-                <div className="col-lg-4 p-0 todo text-center">
-                    <h1>TODO List</h1>
-                    <input type="text" className=' imput' placeholder='Add item'/>
-                    <button type='submit' ><h2>+</h2></button>
-                </div>
-              <div className="col-lg-4 p-0"></div>
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState(['cbasch', 'cjhsdfcgdsf']);
+
+
+  const addTodo = () => {
+    if (input.length < 1) return alert('fillup text first !!')
+    setTodos((prevTodos) => {
+      return [
+        ...prevTodos,
+        input
+      ]
+    });
+    setInput('');
+  };
+
+  const removeTodo = (index) => {
+    let f = todos.filter((t, i) => i !== index);
+    setTodos(f);
+  }
+
+
+  return (
+    <>
+      <section id='todo'>
+        <div className="container">
+          <div className="row p-0">
+            <div className="col-lg-4 p-0"></div>
+            <div className="col-lg-4 p-0 todo text-center">
+              <h1>TODO List</h1>
+              <input type="text" className='imput' value={input} onChange={(e) => setInput(e.target.value)} placeholder='Add item' />
+              <button onClick={addTodo} className="btn btn-primary btn-sm rounded-0" type='button'>+</button>
+              <div className="p-3">
+                <ul className='text-start'>
+                  {
+                    todos.map((t, i) => {
+                      return <li>{t} <span className='btn btn-danger btn-sm' onClick={() => removeTodo(i)}>X</span> </li>;
+                    })
+                  }
+                </ul>
+              </div>
+
             </div>
+            <div className="col-lg-4 col-md-4 p-0"></div>
           </div>
-        </section>
-      </>
+        </div>
+      </section>
+    </>
   )
 }
 
